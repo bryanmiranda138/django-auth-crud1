4ede6aa86494348485f667fb39f0586c21ab10f8
bryanmiranda138 <100953022+bryanmiranda138@users.noreply.github.com>
2024-03-01 14:58:28 -0600

    seg

---
+++ task/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Task
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model= Task
+        fields=['title','description','important']
+        widgets={'title':forms.TextInput(attrs={'class':'form-control','placeholder':'Escribe un Titulo'}),
+                 'description':forms.Textarea(attrs={'class':'form-control','placeholder':'Escribe una Descripcion'}),
+                 'important':forms.CheckboxInput(attrs={'class':'form-check-input text-center'}),}
